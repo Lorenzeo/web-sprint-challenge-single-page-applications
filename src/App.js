518c8c471e757591f86c65850a27042a232affda
unknown <lorenzo.lgayle@gmail.com>
2022-02-25 06:19:41 -0500

    Task 2 section 2 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+import {Switch, Route, Link} from 'react-router-dom'
+import OrderForm from '.components/OrderForm'
 
 const App = () => {
+
+
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <div className='App'>
+      <header>
+        <Link to="/">Home</Link>
+      </header>
+      <Switch>
+          <Route exact path="/">
+            <h1>Lambda Eats</h1>
+            <p>We Code!, We BAKE!, We DELIVER!</p>
+          </Route>
+          <Route path="/pizza">
+            <h2>Order your pizza here!</h2>
+            <OrderForm/>
+          </Route>
+          
+      </Switch>
+      
+    </div>
     </>
   );
 };
---
+++ src/components/OrderForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderForm = () =>{
+
+    return(
+            <>
+            order form
+            </>
+    )
+}
+
+export default OrderForm
